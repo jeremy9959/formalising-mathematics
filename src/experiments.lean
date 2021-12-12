e9b8dfaa6d35520857fedd88824b7cf20998410d
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2021-12-12 17:11:08 -0500

    more progress

--- src/experiments.lean
+++ src/experiments.lean
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-
-
-
-variables p q r : Prop
-
-constant Proof : Prop -> Type 
-
-constant and_comm : Π p q : Prop, Proof (implies (and p q) (and q p))
-
-#check and_comm p q 
-
-#check Proof p 
-
-
-
-
-
-
-
-
-
-theorem t1 : p → q → p :=
-assume hp : p,
-assume hq : q,
-show p, from hp
-=======
+import tactic
 variables  p q r s : Prop 
 variable x : p
 variable y : q
@@ -40,13 +14,14 @@ example (h : p ∧ q) : q ∧ p :=⟨ h.right, h.left ⟩
 
 
 theorem t₄ (h : p ∨ q) : q ∨ p :=
-or.elim h
+h.elim
   (assume hp : p,
     show q ∨ p, from or.intro_right q hp)
   (assume hq : q,
     show q ∨ p, from or.intro_left p hq)
 
-#check t₄
+
+
 
 
  
@@ -70,4 +45,82 @@ theorem t₇ : (p ∧ ¬ q) → ¬ (p → q) :=
   hpanq.right (hpq hpanq.left)
     
 open classical
->>>>>>> f5ce07d97373c97b23b05da322cc073b9d351772
+
+
+
+example :  (p ∧ q) → q :=
+assume hpq,
+  show q, from and.right hpq
+
+
+    
+example : ((p ∨ q) ∧ ¬ q) → p :=
+assume hpqnq,
+  or.elim hpqnq.left
+    (assume hp, show p, from hp)
+    (assume hq, 
+     show p, from false.elim ((hpqnq.right) hq))
+
+   
+theorem t₈ : (p → (q ∨ r)) → ((p → q) ∨ (p → r)) :=
+begin
+intro hpqr,
+by_cases hp : p,
+{ cases hpqr hp,
+{ left, intro, assumption },
+{ right, intro, assumption } },
+{ left, intro, contradiction },
+end 
+
+theorem t₉  : (p → (q ∨ r)) → ((p → q) ∨ (p → r)) :=
+begin
+intro hpqr,
+by_cases hp : p,
+  cases hpqr hp,
+  left, intro, assumption,
+  right, intro, assumption,
+left, intro, contradiction,
+end
+
+
+example : ¬ p → (p → q) :=
+begin
+intro hnp,
+intro hp,
+exact (hnp hp).elim,
+end
+
+theorem t₁₀ : (p → (q ∨ r)) → ((p → q) ∨ (p → r)) :=
+assume hpqr,
+by_cases
+  (assume hp: p, or.elim (hpqr hp)
+    (assume hq : q,  or.intro_left (p→ r) (assume hp : p, hq))
+    (assume hr : r,  or.intro_right (p→ q) (assume hp : p, hr))
+  )
+  (assume hnp : ¬ p,  
+   or.intro_right (p→ q)(assume hp : p, absurd hp hnp)
+  )
+
+theorem t₁₁ : ((p→ q) ∨ (p → r)) → (p → (q ∨ r)) :=
+begin
+  intro hpqpr,
+  cases hpqpr with hpq hpr,
+    {intro hp,
+    have hq := hpq hp,
+    left,
+    assumption},
+    {intro hp,
+    have hr := hpr hp,
+    right,
+    assumption
+    },
+end
+
+
+theorem t₁₂ : ((p → q) ∨ (p → r)) → (p → (q ∨ r)) :=
+assume hpqpr,
+hpqpr.elim
+  (assume hpq : p→ q,  (assume hp : p,  or.intro_left r (hpq hp)))
+  (assume hpr : p→ r,  (assume hp : p, or.intro_right q (hpr hp)))
+
+
