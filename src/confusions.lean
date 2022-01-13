fed1c9f7961b13cbeba2e11d88dabc15bedc9f80
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2022-01-13 08:07:50 -0500

    more confusions

---
+++ src/confusions.lean
@@ -0,0 +1,10 @@
+/- 
+Ten confusing things in lean. 
+
+- implicit arguments 
+- Π (a:α), β  is the same as α → β. 
+- a function is a term of a Π type. 
+- Π (a : α), X → Y means Π (a : α), (X → Y)
+
+-/
+
